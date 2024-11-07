567a6188869694ebf521233b99cd4925253f76f6
heremilton <107191128+heremilton@users.noreply.github.com>
2024-11-07 17:26:37 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -9,33 +9,34 @@
    PASSO 6 - adicinar a classe ativo no programador selecionado na listagem
 */
 
-//PASSO 1 - precisamos criar uma variável no JS pra trabalhar com a listagem de programadores
+//PASSO 1 - Seleciona todos os elementos da listagem de programadores para aplicar eventos de clique
 const listaSelecaoProgamadores = document.querySelectorAll(".programador");
 console.log(listaSelecaoProgamadores);
 
-//PASSO 2 - Identificar o evento de clique no elemento da listagem
-listaSelecaoProgamadores.forEach(programador =>{
-    programador.addEventListener("click", () =>{
-        //PASSO 3 - remover a classe aberto só do cartão que tá aberto
+//PASSO 2 Itera sobre cada elemento da listagem para adicionar um evento de clique
+listaSelecaoProgamadores.forEach(programador => {
+    programador.addEventListener("click", () => {
+
+        // Passo 3: Localiza o cartão atualmente aberto (com a classe "aberto") e remove essa classe para escondê-lo
         const cartaoProgramadorAberto = document.querySelector(".aberto");
         cartaoProgramadorAberto.classList.remove("aberto");
 
-        //PASSO 4 - ao clicar em um programador da lista pegamos o id desse programador pra saber qual cartão abrir
+        // Passo 4: Obtém o ID do programador clicado para identificar qual cartão deve ser exibido
         const idProgramadorSelecionado = programador.attributes.id.value;
-        const idDoCartaoProgramadorParaAbrir = "cartao-" 
-        + idProgramadorSelecionado;
+        const idDoCartaoProgramadorParaAbrir = "cartao-" + idProgramadorSelecionado;
+        
+        // Seleciona o cartão correspondente ao programador clicado e adiciona a classe "aberto" para exibi-lo
         const cartaoProgramadorParaAbrir = document.getElementById(idDoCartaoProgramadorParaAbrir);
         cartaoProgramadorParaAbrir.classList.add("aberto");
 
-        //PASSO 5 - remover a classe ativo que marca o programador selecionado na listagem
+        // Passo 5: Encontra o programador atualmente selecionado (com a classe "ativo") na listagem e remove essa classe
         const programadorAtivoNaListagem = document.querySelector(".ativo");
         programadorAtivoNaListagem.classList.remove("ativo");
 
-        //PASSO 6 - adicinar a classe ativo no programador selecionado na listagem
+        // Passo 6: Adiciona a classe "ativo" ao programador que foi clicado para destacá-lo na listagem
         const programadorSelecionadoNaListagem = document.getElementById(idProgramadorSelecionado);
         programadorSelecionadoNaListagem.classList.add("ativo");
+    });
+});
 
-
-    })
-})
 
